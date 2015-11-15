76cb71f0c43853a15350814bf00b801309da2ab1
Administrator <admin@local.host>
2015-11-15 19:21:25 +0100

    Generate Projects Loopback and Angular

---
+++ client/app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
